92e39b6281bbe38dfb8686e1e518406fd50aa106
Sandeep Sarde <sandeeps@Administrators-MacBook-Pro.local>
2021-02-14 15:44:18 +0100

    Initial commit

---
+++ sanlistingapp/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class SanlistingappConfig(AppConfig):
+    name = 'sanlistingapp'
---
+++ sanlistingapp/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+
+# Create your views here.
+def todoView(request):
+    return render(request, 'todo.html')
+    
